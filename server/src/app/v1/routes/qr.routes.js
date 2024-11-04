862c403751d05008db5ba6b73d40385e95d1f80c
revathipathilanka347 <revathipathilanka347@gmail.com>
2024-11-04 19:46:34 +0530

    routes updated

--- server/src/app/v1/routes/qr.routes.js
+++ server/src/app/v1/routes/qr.routes.js
@@ -1,12 +1,20 @@
-import { uploadData,sendCertificate,test } from "../controllers/qr.controller.js"
+import { uploadData,getDetails,sendCertificate,test } from "../controllers/qr.controller.js"
 import express from 'express'
 const router = express.Router();
 
-router.route('/upload')
+const upload = multer({ 
+	storage: storage,
+	limits: { fileSize: 1024 * 1024 * 512 } 
+});
+
+router.route('/uploadData', upload.single('image'))
 	.post(uploadData);
 
 router.route('/sendCertificate')
 	.post(sendCertificate);
+
+router.route('/getDetails')
+	.get(getDetails)
 
 router.route("/test")
 	.get(test)
