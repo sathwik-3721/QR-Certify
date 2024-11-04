4a77041d22dbcbbc3592ede8b61ea6db3d26747b
revathipathilanka347 <revathipathilanka347@gmail.com>
2024-11-04 19:33:49 +0530

    mail finished

--- server/src/app/v1/routes/qr.routes.js
+++ server/src/app/v1/routes/qr.routes.js
@@ -1,8 +1,14 @@
-import { uploadData } from "../controllers/qr.controller.js"
+import { uploadData,sendCertificate,test } from "../controllers/qr.controller.js"
 import express from 'express'
 const router = express.Router();
 
 router.route('/upload')
 	.post(uploadData);
+
+router.route('/sendCertificate')
+	.post(sendCertificate);
+
+router.route("/test")
+	.get(test)
 
 export default router;
